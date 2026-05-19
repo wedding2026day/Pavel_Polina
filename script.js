8da9c6f2bbf741d98a7d34d8cea6a0126da7b5f0
wedding2026day <marina.plyasheva@mail.ru>
2026-05-19 21:27:55 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* =========================
+   АНИМАЦИИ
+========================= */
+
 const animatedItems = document.querySelectorAll('.animate');
 
 const observer = new IntersectionObserver((entries) => {
@@ -14,4 +18,38 @@ const observer = new IntersectionObserver((entries) => {
 
 animatedItems.forEach((item) => {
   observer.observe(item);
+});
+
+
+/* =========================
+   ОТПРАВКА ФОРМЫ
+========================= */
+
+const form = document.getElementById('wedding-form');
+const successText = document.querySelector('.form-success');
+
+form.addEventListener('submit', async function(e){
+
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const response = await fetch(
+    'https://api.web3forms.com/submit',
+    {
+      method:'POST',
+      body:formData
+    }
+  );
+
+  const result = await response.json();
+
+  if(result.success){
+
+    successText.style.display = 'block';
+
+    form.reset();
+
+  }
+
 });
